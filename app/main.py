47b73b91b0c227b0780b06e7abe1a308e180e46e
sunil kumar <sunilkumarmekalaboina@gmail.com>
2025-12-26 15:54:12 -0600

    Add database config and Student model

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from app.database import engine
+from app.models import student
+
+student.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Student Management System API")
 
